01320abb8c6816cd3f34e8f5c5ce7c732b245a38
David A. Vargas <davidavargas.dev@gmail.com>
2025-03-19 15:57:59 -0500

    added dark & light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/ModeToggle" 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -8,7 +10,7 @@ const geistSans = Geist({
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+  variable: "--font-geist-mono", 
   subsets: ["latin"],
 });
 
@@ -27,7 +29,19 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+
+            <div>
+            <ModeToggle/>
+          </div>
+          </ThemeProvider>
+        
       </body>
     </html>
   );
