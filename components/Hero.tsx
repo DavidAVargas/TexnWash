8f5aaea93126f7f7339d1006bdc40e84b2a134fe
David A. Vargas <davidavargas.dev@gmail.com>
2025-05-03 18:23:16 -0500

    Refactor Hero component layout and improve content structure

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,30 @@
 import Image from "next/image";
 import Container from "./Container";
+import { Button } from "./ui/button";
 
 export default function Hero() {
   return (
-    <section>
+    <section className="pt-20">
       <Container>
-        <div>
-          <h1>Title</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptatibus.
-          </p>
+        <div className="flex flex-col md:flex-row items-center justify-center   gap-50">
+          <div className="text-left max-w-xl">
+            <h1 className="text-8xl md:text-8xl font-bold text-brand mb-4">
+              Because First Impressions{''}
+              <span style={{ color: '#c3b091'}} > Start at the Curb </span>
+            </h1>
+            <p className="text-2xl text-black">
+              Your neighbors will ask who cleaned it — trust us.
+            </p>
+            <div>
+          <Button className="bg-brand text-white mt-8">
+            <a href="/contact">Free Quote</a>
+          </Button>
+          <p className="mt-2 text-lg">Get an estimate for any service you need hassle free.</p>
         </div>
-        <div>
-          <Image src="/baibfiubIEb.jpg" width={500} height={500} alt="Hero Image" />
+          </div>
+          <div>
+            <Image src="/images/logo.png" width={500} height={500} alt="Hero Image" />
+          </div>
         </div>
       </Container>
     </section>
